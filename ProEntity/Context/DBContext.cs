1ec32f902c975f812a4776aad9df2a85e2d5ee7c
yildirimynsemre <yildirimynsemre@gmail.com>
2020-12-17 21:00:37 +0300

    Add project files.

---
+++ ProEntity/Context/DBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEntity.Context
+{
+    public class DBContext : DbContext
+    {
+        public DBContext() : base("BlogContext")
+        {
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // İlişkileri kuruyoruz one-to-many olarak.
+        }
+    }
+}
